b9dd4075157f45d7124d225f5a03d49af4b4c6b9
Sumit Kumar Verma <93787974+KylarSec@users.noreply.github.com>
2025-12-31 01:20:38 +0530

    add basic tests

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -6,29 +6,79 @@ import {Test, console} from "forge-std/Test.sol";
 
 import {FundMe} from "../src/FundMe.sol";
 
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
 contract FundMeTest is Test {
+    address user1 = makeAddr("user1");
+
+    uint256 constant FUND_VALUE = 1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     FundMe fm;
 
     function setUp() external {
-        //us -> FundMeTest -> FundMe
-        fm = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fm = deployFundMe.run();
+        vm.deal(user1, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public view {
         assertEq(fm.MINIMUM_USD(), 5e18);
     }
 
-    /**
-     *The test is testOwnerisDeployer because this test contract will deploy the
-     * FundeMe contract so this test contract will be the owner.
-     */
-    function test_OwnerIsDeployer() public {
-        //address(this) -> address of this contract
-        assertEq(fm.i_owner(), address(this));
+    function test_FundFailswithoutSuffucientEth() public {
+        vm.expectRevert();
+        fm.fund();
+    }
+
+    function test_FundPasswhenSufficientEth() public {
+        fm.fund{value: 6e18}();
+    }
+
+    // function test_OwnerIsDeployer() public {
+    //     //address(this) -> address of this contract
+    //     assertEq(fm.i_owner(), address(this));
+    // }
+
+    function test_OwnerIsMsgSender() public {
+        assertEq(fm.i_owner(), msg.sender);
     }
 
     function test_CheckPriceFeedVersion() public {
-        uint256 version = fm.GetVersion();
-        assertEq(version, 4);
+        if (block.chainid == 11155111) {
+            uint256 version = fm.GetVersion();
+            assertEq(version, 4);
+        } else if (block.chainid == 31337) {
+            uint256 version = fm.GetVersion();
+            assertEq(version, 4);
+        }
+    }
+
+    function testFundUpdatesFundData() public {
+        vm.prank(user1); //The next Transaction will be sent by user1
+        fm.fund{value: FUND_VALUE}();
+        uint256 amountFunded = fm.getAddressToAmountFunded(user1);
+        assertEq(amountFunded, FUND_VALUE);
+    }
+
+    function testWithdrawFailsIfNotOwner() public {
+        vm.expectRevert();
+        fm.withdraw();
+    }
+
+    function testOnlyOwnerCanWithdraw() public {
+        address owner = fm.i_owner();
+
+        vm.prank(owner);
+        fm.withdraw();
+    }
+
+    function testBalanceZeroAfterWithdrawByOwner() public {
+        address owner = fm.i_owner();
+        vm.prank(owner);
+        fm.withdraw();
+
+        //assert balance is zero
+        assertEq(address(fm).balance, 0);
     }
 }
